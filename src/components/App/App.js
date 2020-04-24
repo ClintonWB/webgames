0b006dbd959bc2779dc46632a8cfb86be92d7f07
= <clinton@clintonbradford.com>
2020-04-24 16:37:41 -0400

    Bring imported/generated files up to style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from '../../images/logo.svg';
 import './App.css';
 import Form from '../Form/Form.js';
 import firebase from 'firebase';
-import firebaseConfig from '../../config';firebase.initializeApp(firebaseConfig);
+import firebaseConfig from '../../config';
+firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
-  constructor(props) {
-    super(props);    this.state = {
-      user: null,
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: null,
+        }
     }
-  }  componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      this.setState({ user });
-    });
-  }  handleSignIn() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  }  handleLogOut() {
-    firebase.auth().signOut();
-  }  render() {
-    return (
-      <div className="app">
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({
+                user
+            });
+        });
+    }
+    handleSignIn() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider);
+    }
+    handleLogOut() {
+        firebase.auth().signOut();
+    }
+    render() {
+        return (
+            <div className="app">
         <div className="app__header">
           <img src={logo} className="app__logo" alt="logo" />
           <h2>
@@ -46,6 +54,7 @@ class App extends Component {
           <Form user={this.state.user} />
         </div>
       </div>
-    );
-  }
-}export default App;
+        );
+    }
+}
+export default App;
--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
-import './Message.css';export default class Message extends Component {
-  render() {
-    return (
-      <div className="message">
+import './Message.css';
+export default class Message extends Component {
+    render() {
+        return (
+            <div className="message">
                 <span className="message__author">
-                    {this.props.message.userName}:
-                </span>
-        {this.props.message.message}
-      </div>
-    )
-  }
+                    {this.props.message.userName}
+                </span>:
+                <span className="message__body">
+                    {this.props.message.message}
+                </span>    
+            </div>
+        )
+    }
 }
