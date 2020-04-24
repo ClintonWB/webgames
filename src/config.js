81f386ba1bbfca65864cffc66ff80a2a4e9b8be7
= <clinton@clintonbradford.com>
2020-04-24 18:24:52 -0400

    Update api config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 export default {
-  apiKey: "AIzaSyAaODb7bCoZPvV4gdVyG_sV_Lc1_GuVdwg",
-  authDomain: "react-intro-37cd1.firebaseapp.com",
-  databaseURL: "https://react-intro-37cd1.firebaseio.com",
-  projectId: "react-intro-37cd1",
-  storageBucket: "react-intro-37cd1.appspot.com",
-  messagingSenderId: "181293593583"
+  apiKey: "AIzaSyDWhExNdkYQ_TemW4Rp7K_tH3nS3wEw6Ls",
+  authDomain: "games-eax51.firebaseapp.com",
+  databaseURL: "https://games-eac51.firebaseio.com",
+  projectId: "",
+  storageBucket: "games-eac51.appspot.com",
 };
