0253f08ab13ad7088845a268cc076e96d810f307
= <clinton@clintonbradford.com>
2020-04-24 18:26:26 -0400

    Update api config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
-export default {
+const firebaseConfig = {
   apiKey: "AIzaSyDWhExNdkYQ_TemW4Rp7K_tH3nS3wEw6Ls",
-  authDomain: "games-eax51.firebaseapp.com",
+  authDomain: "games-eac51.firebaseapp.com",
   databaseURL: "https://games-eac51.firebaseio.com",
-  projectId: "",
+  projectId: "games-eac51",
   storageBucket: "games-eac51.appspot.com",
+  messagingSenderId: "130501765958",
+  appId: "1:130501765958:web:aeef0d74998de08094d347",
+  measurementId: "G-R9MDXFXQL0"
 };
